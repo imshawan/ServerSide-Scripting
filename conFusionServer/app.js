c5dd93acccd4398797d7c5b5c9bd2fe5ada489c8
Shawan Mandal <shawan.sm049@gmail.com>
2021-02-21 16:55:06 +0530

    Express-Generator API

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -7,6 +7,10 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
+
 var app = express();
 
 // view engine setup
@@ -21,6 +25,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use('/dishes', dishRouter);
+//app.use('/dishes/:dishID', dishRouter);
+
+app.use('/promotions', promoRouter);
+//app.use('/promotions/:promoID', promoRouter);
+
+app.use('/leaders', leaderRouter);
+//app.use('/leaders/:leaderId', leaderRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
