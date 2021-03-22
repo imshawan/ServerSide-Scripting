b7ccda7e52b6bfedf723bf676417e12db5e590b3
Shawan Mandal <shawan.sm049@gmail.com>
2021-03-22 17:26:00 +0530

    Completed Week 3 Assignments

--- conFusionServer-Week-3/routes/users.js
+++ conFusionServer-Week-3/routes/users.js
@@ -12,8 +12,16 @@ router.use(bodyParser.json());
 
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, function(req, res, next) {
+  User.find({}, (err, users) => {
+    if (err) {
+      return next(err);
+    } else {
+      res.statusCode = 200;
+      res.setHeader('Content_type', 'application/json');
+      res.json(users);
+    }
+  })
 });
 
 
@@ -55,18 +63,18 @@ router.post('/login', passport.authenticate('local'), (req, res) => {
   res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });
 
-router.get('/logout', (req, res) => {
-  if (req.session) {
-    req.session.destroy();
-    res.clearCookie('session-id');
-    res.redirect('/');
-  }
-  else {
-    var err = new Error('You are not logged in!');
-    err.status = 403;
-    next(err);
-  }
-});
+router.get('/logout', (req, res, next) => {
+    if (req.session) {
+      req.session.destroy();
+      res.clearCookie('session-id');
+      res.redirect('/');
+    }
+    else {
+      var err = new Error('You are not logged in!');
+      err.status = 403;
+      next(err);
+    }
+  });
 
 
 module.exports = router;
