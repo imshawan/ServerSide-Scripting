2cdcb41472f58fef9c90ae1682756c5d72bebead
Shawan Mandal <shawan.sm049@gmail.com>
2021-04-26 22:03:45 +0530

    HTTPS

---
+++ conFusionServer-Week-4/routes/leaderRouter.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const mongoose = require('mongoose');
+const Leaders = require('../models/leaders');
+var authenticate = require('../authenticate');
+
+const leaderRouter = express.Router();
+
+leaderRouter.use(bodyparser.json());
+leaderRouter.route('/(:leaderID)?')
+//leaderRouter.route('/')
+
+.get((req, res, next) => {
+	if(req.params.leaderID){
+		Leaders.findById(req.params.leaderID)
+		.then((leader) => {
+			res.statusCode = 200;
+			res.setHeader('Content-Type', 'application/json');
+			res.json(leader)
+		}, (err) => next(err))
+		.catch((err) => next(err));
+	}
+    else {
+	Leaders.find({})
+	.then((leaders) => {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'application/json');
+		res.json(leaders);
+	}, (err) => next(err))
+	.catch((err) => next(err));
+    }
+})
+.post(authenticate.verifyUser, (req, res, next) => {
+	if(req.params.leaderID){
+		res.statusCode = 403;
+		res.end('POST operation not supported on /leaders/' + req.params.leaderID);
+	}
+    else {
+	Leaders.create(req.body)
+	.then((leader) => {
+		console.log('Leader Created', leader);
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'application/json');
+		res.json(leader);
+	}, (err) => next(err))
+	.catch((err) => next(err));
+    }
+})
+.put(authenticate.verifyUser, (req, res, next) => {
+	if(req.params.leaderID){
+		Leaders.findByIdAndUpdate(req.params.leaderID, {
+			$set: req.body
+		}, { new: true })
+		.then((leader) => {
+			console.log('leader Updated', leader);
+			res.statusCode = 200;
+			res.setHeader('Content-Type', 'application/json');
+			res.json(leader);
+		}, (err) => next(err))
+		.catch((err) => next(err));
+	}
+    else {
+	res.statusCode = 403;
+	res.end('PUT operation bot supported on leaders');
+    }
+})
+.delete(authenticate.verifyUser, (req, res, next) => {
+	if(req.params.leaderID){
+		Leaders.findByIdAndRemove(req.params.leaderID)
+		.then((resp) => {
+			res.statusCode = 200;
+			res.setHeader('Content-Type', 'application/json');
+			res.json(resp);
+		}, (err) => next(err))
+		.catch((err) => next(err));
+	}
+    else {
+	Leaders.remove({})
+	.then((resp) => {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'application/json');
+		res.json(resp);
+	}, (err) => next(err))
+	.catch((err) => next(err));
+    }
+});
+
+module.exports = leaderRouter;
---
+++ conFusionServer-Week-4/routes/promoRouter.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const Promotions = require('../models/promotions');
+const mongoose = require('mongoose');
+var authenticate = require('../authenticate');
+
+const promoRouter = express.Router();
+
+promoRouter.use(bodyparser.json());
+promoRouter.route('/(:promoID)?')
+//promoRouter.route('/')
+
+.get((req,res,next) => {
+    if(req.params.promoID) {
+        Promotions.findById(req.params.promoID)
+        .then((promotion) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(promotion);
+        }, (err) => next(err))
+        .catch((err) => next(err));
+    }
+    else {
+    Promotions.find({})
+    .then((promotions) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(promotions);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+    }
+})
+.post(authenticate.verifyUser, (req, res, next) => {
+    if (req.params.promoID){
+        res.statusCode = 403;
+        res.end('POST operation not supported on /promotions/'+ req.params.promoID);
+    }
+    else {
+    Promotions.create(req.body)
+    .then((promotion) => {
+        console.log('Promotion Created ', promotion);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(promotion);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+    }
+})
+.put(authenticate.verifyUser, (req, res, next) => {
+    if(req.params.promoID){
+        Promotions.findByIdAndUpdate(req.params.promoID, {
+            $set: req.body
+        }, { new: true })
+        .then((promotion) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(promotion);
+        }, (err) => next(err))
+        .catch((err) => next(err));
+    }
+    else {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /promotions');
+    }
+})
+.delete(authenticate.verifyUser, (req, res, next) => {
+    if(req.params.promoID) {
+        Promotions.findByIdAndRemove(req.params.promoID)
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+    }
+    else {
+    Promotions.remove({})
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err));  
+    }  
+});
+
+
+module.exports = promoRouter;
