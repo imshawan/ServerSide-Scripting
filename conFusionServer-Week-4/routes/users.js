a5500e7eed3d9f69090018f533563df87be1aa55
Shawan Mandal <shawan.sm049@gmail.com>
2021-04-29 11:36:20 +0530

    Cors

--- conFusionServer-Week-4/routes/users.js
+++ conFusionServer-Week-4/routes/users.js
@@ -5,6 +5,7 @@ var passport = require('passport');
 
 const bodyParser = require('body-parser');
 var User = require('../models/user');
+const cors = require('./cors');
 
 var authenticate = require('../authenticate');
 
@@ -12,7 +13,7 @@ router.use(bodyParser.json());
 
 
 /* GET users listing. */
-router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, function(req, res, next) {
+router.get('/', cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, function(req, res, next) {
   User.find({}, (err, users) => {
     if (err) {
       return next(err);
@@ -25,7 +26,7 @@ router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, function(req,
 });
 
 
-router.post('/signup', (req, res, next) => {
+router.post('/signup', cors.corsWithOptions, (req, res, next) => {
   User.register(new User({username: req.body.username}), 
     req.body.password, (err, user) => {
     if(err) {
@@ -55,7 +56,7 @@ router.post('/signup', (req, res, next) => {
   });
 });
 
-router.post('/login', passport.authenticate('local'), (req, res) => {
+router.post('/login', cors.corsWithOptions, passport.authenticate('local'), (req, res) => {
 
   var token = authenticate.getToken({_id: req.user._id});
   res.statusCode = 200;
