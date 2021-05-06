73d4680500fe74039cd763eb6f3c23168ac4d66e
Shawan Mandal <shawan.sm049@gmail.com>
2021-05-06 10:52:21 +0530

    Added Final Assignment

--- conFusionServer-Week-4/config.js
+++ conFusionServer-Week-4/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '1415654242111576',
+        clientSecret: '1899318bf5d4b0023532098925bfd13d'
+    }
 }
